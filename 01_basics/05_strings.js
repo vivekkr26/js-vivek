4fe630fea3822c94ffa6bc94c8cb3667d03b5af8
vivek kumar kasuhal <124205411+vivekkr26@users.noreply.github.com>
2024-10-19 03:03:44 +0000

    code

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,29 @@
+const name ="vivek"
+const repoCount=60
+
+//console.log(name+repoCount+"value")
+
+//console.log(`hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String(`vivek-hc-com`)
+//console.log(gameName[0])
+//console.log(gameName.___proto___);
+//console.log(gameName.charAt(2))
+//console.log(gameName.indexOf('v'))
+//const newString =gameName.substring(0,4)
+//console.log(newString)
+
+const astring=gameName.slice(-8,4)
+console.log(astring)
+
+
+const newStringOne="        vivek        "
+console.log(newStringOne)
+console.log(newStringOne.trim())
+
+const url = "www.insta.com"
+console.log(url.replace('%20','-'))
+
+console.log(url.includes("sundar"))
+
+console.log(gameName.split('-'))
