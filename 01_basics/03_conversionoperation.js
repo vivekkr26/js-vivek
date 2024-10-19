84db8a372cde4bc301198e867028734b376f57b5
vivek kumar kasuhal <124205411+vivekkr26@users.noreply.github.com>
2024-10-19 01:54:37 +0000

    code

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,72 @@
+let score ="vivek"
+//console.log(typeof score)
+//console.log(typeof(score))
+
+let valueInNumber = Number(score)
+//console.log( score)
+//console.log(typeof valueInNumber)
+//console.log( valueInNumber)
+
+//"33"=> 33
+//"33a"=> NaN
+//true =>1; flase=>0
+
+let isLoggedIn = "vivek "
+let booleanIsLoggedIn= Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn)
+
+//1= true
+//0=false
+//""= false
+//"vivek"=true
+
+
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+/*
+console.log(stringNumber)
+console.log(typeof stringNumber)
+console.log(typeof someNumber)
+*/
+
+let value = 3
+let negvalue = -value 
+//console.log(negvalue)
+
+
+
+/*
+console.log(2+2)
+console.log(2-2)
+console.log(2*2)
+console.log(4**3)
+console.log(2/3)
+console.log(2%3)
+*/
+let str1 = "hiii"
+let str2 = "vivek"
+
+let str3 = str1+str2
+//console.log(str3)
+/*
+console.log("1"+2)
+console.log(1+"2")
+console.log("1"+2+2)
+console.log(1+2+"2")
+
+console.log( (3 + 4) * 5 % 3);
+*/
+
+console.log(+true)
+console.log(+"")
+
+
+
+let gameChanger=100
+++gameChanger;
+console.log(gameChanger)
+
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
