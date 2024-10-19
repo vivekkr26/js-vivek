7cc5ae718a759a496f4f2340a00c8c56dad5feda
vivek kumar kasuhal <124205411+vivekkr26@users.noreply.github.com>
2024-10-19 03:28:31 +0000

    code

---
+++ 01_basics/06_num_maths.js
@@ -0,0 +1,46 @@
+const score =200
+//console.log(score)
+
+const balance = new Number (100)
+//console.log(balance)
+
+//console.log(balance.toString().length)
+
+//console.log(balance.toFixed(2))
+
+const otherNumber=123.8966
+//console.log(otherNumber.toLocaleString('en-IN'))
+
+const hundreds =100000
+//console.log(hundreds.toLocaleString)
+
+//+++++++++++++++++++  MATHS  ++++++++++++++++++++++
+
+//console.log(Math)
+//console.log(Math.abs(-4))
+//console.log(Math.round(4.6))
+//console.log(Math.ceil(8.9))
+//console.log(Math.floor(7.8))
+//console.log(Math.min(1,2,3,6,6))
+//console.log(Math.max(9,90,80,70))
+
+console.log(Math.random())
+console.log((Math.random()*10)+1)
+console.log(Math.floor(Math.random()*10)+1)
+
+
+const min =10
+const max=20
+console.log(Math.floor(Math.random()*(max-min+1))+min)
+
+
+
+
+
+
+
+
+
+
+
+
