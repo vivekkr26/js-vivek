8e9ac10c81b58b8e70f9895b8ea657599b5d9e52
vivek kumar kasuhal <124205411+vivekkr26@users.noreply.github.com>
2024-10-19 02:32:46 +0000

    code

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,23 @@
+/*
+console.log (1>2)
+console.log(2>=1)
+console.log (2<1)
+console.log(2==1);
+console.log (2 !=1);
+
+
+
+console.log("2">1)
+console.log("02">1)
+*/
+
+console.log(null>0)
+console.log(null==0)
+console.log(null >=0)// here the js converets the null =0 so 0>=0 becomes true 
+
+console.log(undefined ==0)
+console.log(undefined > 0)
+console.log(undefined<0)
+
+
+console.log("2"===2)
